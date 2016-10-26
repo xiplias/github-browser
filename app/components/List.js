9d5fade437e06e0d04dcf5c67bd293b64b32079d
Anders Hansen <xiplias@gmail.com>
2016-10-26 14:53:29 +0200

    Improve global font-size

--- app/components/List.js
+++ app/components/List.js
@@ -4,7 +4,7 @@ const List = styled.div`
   border-right: 1px #ccc solid;
   height: 100%;
   overflow-y: scroll;
-  font-size: 0.9em;
+  font-size: 0.85em;
 `;
 
 export default List;
--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -4,7 +4,6 @@ const ListItem = styled.div`
   padding: 10px;
   border-bottom: 1px #ccc solid;
   cursor: pointer;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
 `;
 
 export default ListItem;
